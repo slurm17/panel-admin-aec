0651aa6eae650d7ef448df50abacb7e9dc25989c
Eric Busten <eric.s.busten@gmail.com>
2025-08-30 12:44:07 -0300

    fist commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  server: {
+      port: 5173,
+      open: true,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      sourcemap: false, // no exponer código fuente original en producción
+    },
+})
